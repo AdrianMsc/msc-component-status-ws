275545af1d2ef98bbbe1e7296e43ee67f0f5a1bd
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-02-04 11:36:26 -0600

    comment required remove

--- neon-index.js
+++ neon-index.js
@@ -90,7 +90,7 @@ app.post("/categories/:category/components", async (req, res) => {
   const { category } = req.params;
   const { name, comment, figma, guidelines, cdn, storybook } = req.body;
 
-  if (!name || !comment) {
+  if (!name) {
     return res.status(400).json({ error: "Required data incomplete." });
   }
 
